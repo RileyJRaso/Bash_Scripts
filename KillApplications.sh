c33020cfaae7d46d04f9a5c80636e2f8e0514a93
Riley Raso <rileyraso@Rileys-MBP.gv.shawcable.net>
2019-03-12 20:58:32 -0700

    Commented KillApplications.sh

--- KillApplications.sh
+++ KillApplications.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
+#Contains a list of applications that should all be closed
+#Checks if the application has any processes that are currently running
+#if there are applications open kill them
+#if there is no instances open display a message to terminal
+#Tell the terminal when the process is complete
+#(this is a simple modular for a larger project)
+
+#list of applications (update as needed)
 declare -a List_Of_Applications=("Google Chrome" "Preview" "Excel" "iTunes" "OneNote"  "java" "WhatsApp" "Messages" "Atom") 
 
+#Message to terminal
 echo ""
 echo "---Clearing Current Applications---"
 echo ""
 
 for element in "${List_Of_Applications[@]}"
 do
-	
+	#Message to terminal
 	echo ""
 	echo "---Searching for $element---"
-
+	
+	#checks if current Application is running
 	Application_Open=$(ps -Ac | grep "$element")
-
+	
+	#if open tell terminal and close it
+	#else tell terminal it is not running
 	if [ ! -z "$Application_Open" ]; then
 		Application_To_Kill=$(ps -Ac | pgrep "$element")
 		echo "---Killing "$element"---"
